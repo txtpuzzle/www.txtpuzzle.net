ebfa67ea48cdd4a3beae5bbe771bc49a1e78345a
laurentabbal <laurent@abbal.com>
2022-02-23 09:51:46 +0900

    bug liens

--- resources/views/guest-puzzle.blade.php
+++ resources/views/guest-puzzle.blade.php
@@ -20,10 +20,10 @@
 	<div class="container mt-5 mb-5">
 
         <div class="text-monospace font-weight-bolder text-center" style="font-size:1.5em;color:silver">
-            <a href="/p/{{ strtoupper($puzzle->jeton) }}" target="_blank">www.codepuzzle.io/p/{{ strtoupper($puzzle->jeton) }}</a>
+            <a href="/p/{{ strtoupper($puzzle->jeton) }}" target="_blank">www.txtpuzzle.net/p/{{ strtoupper($puzzle->jeton) }}</a>
         </div>
         <div class="text-center mt-2 mb-4">
-            <img src="https://api.qrserver.com/v1/create-qr-code/?data={{urlencode('www.codepuzzle.io/p/' . $puzzle->jeton)}}&amp;size=100x100" style="width:100px" alt="www.codepuzzle.io/p/{{$puzzle->jeton}}" data-toggle="tooltip" data-placement="right" title="{{__('clic droit + Enregistrer l image sous... pour sauvegarder l image')}}" />
+            <img src="https://api.qrserver.com/v1/create-qr-code/?data={{urlencode('www.txtpuzzle.net/p/' . $puzzle->jeton)}}&amp;size=100x100" style="width:100px" alt="www.txtpuzzle.net/p/{{$puzzle->jeton}}" data-toggle="tooltip" data-placement="right" title="{{__('clic droit + Enregistrer l image sous... pour sauvegarder l image')}}" />
         </div>
 
 		<div class="row pt-3">
@@ -34,16 +34,16 @@
 
                 <a class="btn btn-success btn-sm mb-4" href="{{route('guest-puzzle-creer-get')}}" role="button" style="width:100%;">{{__('créer un nouveau puzzle')}}</a>
 
-                <a href="https://github.com/codepuzzle-io/www.codepuzzle.io/discussions" target="_blank" role="button" class="mt-2 btn btn-light btn-sm text-left text-muted" style="width:100%;opacity:0.8;">
+                <a href="https://github.com/txtpuzzle/www.txtpuzzle.net/discussions" target="_blank" role="button" class="mt-2 btn btn-light btn-sm text-left text-muted" style="width:100%;opacity:0.8;">
                 	<span style="font-size:80%"><i class="fas fa-comment-alt" style="float:left;margin:4px 8px 5px 0px;"></i> {{__('discussions')}} <span style="opacity:0.6;font-size:90%;">&</span> {{__('annonces')}}</span>
                 </a>
 
-                <a href="https://github.com/codepuzzle-io/www.codepuzzle.io/issues/new/choose" target="_blank" role="button"  class="mt-1 btn btn-light text-left btn-sm text-muted" style="width:100%;opacity:0.8;">
+                <a href="https://github.com/txtpuzzle/www.txtpuzzle.net/issues" target="_blank" role="button"  class="mt-1 btn btn-light text-left btn-sm text-muted" style="width:100%;opacity:0.8;">
                 	<span style="font-size:80%"><i class="fas fa-bug" style="float:left;margin:4px 8px 5px 0px;"></i> {{__('signalement de bogue')}} <span style="opacity:0.6;font-size:90%;">&</span> {{__('questions techniques')}}</span>
                 </a>
 
                 <div class="mt-3 text-muted text-monospace pl-1 mb-5" style="font-size:70%;opacity:0.8;">
-                	<span><i class="fa fa-envelope"></i> contact@codepuzzle.io</span>
+                	<span><i class="fa fa-envelope"></i> contact@txtpuzzle.net</span>
                 </div>
 
             </div>
@@ -57,7 +57,7 @@
                             <div class="text-monospace text-muted mb-3 small">
                                 <i class="fas fa-share-alt ml-1 mr-1"></i> {{__('Code à insérer dans un site web')}}
                                 <div class="mt-1" style="margin-left:22px;">
-                                    <input class="form-control form-control-sm" type="text" value='<iframe src="https://www.codepuzzle.io/iframe/{{ strtoupper($puzzle->jeton) }}" width="100%" height="600" frameborder="0"></iframe>' disabled readonly />
+                                    <input class="form-control form-control-sm" type="text" value='<iframe src="https://www.txtpuzzle.net/iframe/{{ strtoupper($puzzle->jeton) }}" width="100%" height="600" frameborder="0"></iframe>' disabled readonly />
                                 </div>
                                 <p class="text-monospace mt-1" style="margin-left:22px;font-size:90%";color:silver>{{__('Remarque : ajuster la valeur de "height" en fonction de la taille du puzzle')}}</p>
                             </div>
